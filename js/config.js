8f5444ec5839b9e0842bab16f8182f8dbc438a4a
Ken760 <nehnaev@gmail.com>
2019-04-28 15:34:26 +0300

    config update

--- js/config.js
+++ js/config.js
@@ -9,18 +9,106 @@ const config = {
                     'Архитектура GPU',
                     'RTX-OPS',
                     'Тактовая частота с ускорением',
-                    'Фреймбуфер',
-                    'Быстродействие памяти'
+                    'Объем памяти, Гбайт',
+                    'Пропускная способность памяти'
                 ],
                 [
                     'Turing',
-                    '76T<',
-                    '1545 MHz<',
-                    '11GB GDDR6<',
-                    '14 Gbps<'
+                    '76T',
+                    '1545 MHz',
+                    '11GB GDDR6',
+                    '616 Gbps'
                 ]
             ],
             video_frame: '<iframe width="600" height="325" src="https://www.youtube.com/embed/eBW-CxOcCPY" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-        }
+        },
+        // PAGE 2 //
+        {
+            name: 'GTX1080Ti',
+            img: '../res/GeForce_GTX_1080t.png',
+            info: [
+                [
+                    'Архитектура GPU',
+                    'NVIDIA CUDA® Cores',
+                    'Тактовая частота с ускорением',
+                    'Объем памяти, Гбайт',
+                    'Пропускная способность памяти'
+                ],
+                [
+                    'Pascal',
+                    '3584',
+                    '1582 MHz',
+                    '11GB GDDR6',
+                    '489 Gbps'
+                ]
+            ],
+            video_frame: ' <iframe width="600" height="325" src="https://www.youtube.com/embed/Owcu3gT6uj0" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        // PAGE 3 //
+        {
+            name: 'RXVEGA 64',
+            img: '../res/RxVega64.png',
+            info: [
+                [
+                    'Архитектура GPU',
+                    'Технологический процесс',
+                    'Тактовая частота с ускорением',
+                    'Объем памяти, Гбайт',
+                    'Пропускная способность памяти'
+                ],
+                [
+                    'VEGA',
+                    '14 nm',
+                    '1546 MHz',
+                    '8GB HBM5',
+                    '484 Gbps'
+                ]
+            ],
+            video_frame: '<iframe width="600" height="325" src="https://www.youtube.com/embed/mutfVxluv_k" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        // PAGE 4 //
+        {
+            name: 'TESLA V100',
+            img: '../res/teslav100.png',
+            info: [
+                [
+                    'Архитектура GPU',
+                    'Технологический процесс',
+                    'Тактовая частота с ускорением',
+                    'Объем памяти, Гбайт',
+                    'Пропускная способность памяти'
+                ],
+                [
+                    'TESLA',
+                    '12 nm',
+                    '1380 MHz',
+                    '16GB HBM2',
+                    '900 Gbps'
+                ]
+            ],
+            video_frame: '<iframe width="600" height="325" src="https://www.youtube.com/embed/Hc7mCSbFpKM" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        // PAGE 5 //
+        {
+            name: 'QUADRO RTX6000',
+            img: '../res/QUADRORTX6000.png',
+            info: [
+                [
+                    'Архитектура GPU',
+                    'RTX-OPS',
+                    'Тактовая частота с ускорением',
+                    'Объем памяти, Гбайт',
+                    'Производительность'
+                ],
+                [
+                    'Turing',
+                    '84T',
+                    '1730 MHz',
+                    '24GB GDDR6',
+                    '16.3 TFLOPS'
+                ]
+            ],
+            video_frame: '<iframe width="600" height="325" src="https://www.youtube.com/embed/bd1L3h9kLq8" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
     ]
 }
